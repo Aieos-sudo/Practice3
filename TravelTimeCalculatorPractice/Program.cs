8f33b1bc26e3a86ecdbf9cfe0875dc5e40dcc70b
Aieos <firered9000@yahoo.com>
2021-11-30 22:23:53 -0600

    added exception handling

--- TravelTimeCalculatorPractice/Program.cs
+++ TravelTimeCalculatorPractice/Program.cs
@@ -6,19 +6,48 @@ namespace TravelTimeCalculatorPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Travel Time Calculator0 \n");
-            Console.Write("Enter Miles: ");
-            string miles = Console.ReadLine();
-            Console.Write("Enter miles per hour: ");
-            string speed = Console.ReadLine();
-            int milesint = Int32.Parse(miles);
-            int speedint = Int32.Parse(speed);
-            int hours = (milesint / speedint);
-            int minutes = (milesint % speedint);
-            Console.WriteLine();
-            Console.WriteLine("Estimated travel time");
-            Console.WriteLine("Hours: " + hours);
-            Console.Write("Minutes: " + minutes);
+            int milesInt = 0;
+            int speedInt = 0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Travel Time Calculator \n");
+                    Console.Write("Enter Miles: ");
+                    string miles = Console.ReadLine();
+                    Console.Write("Enter miles per hour: ");
+                    string speed = Console.ReadLine();
+                    milesInt = Int32.Parse(miles);
+                    speedInt = Int32.Parse(speed);
+                    int hours = (milesInt / speedInt);
+                    int minutes = (milesInt % speedInt);
+                    Console.WriteLine();
+                    Console.WriteLine("Estimated travel time");
+                    Console.WriteLine("Hours: " + hours);
+                    Console.WriteLine("Minutes: " + minutes);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Entered value must be an integer. Error: " + ex.Message);
+                    Console.WriteLine();
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("Entered value must be less than 2.4 billion. Error: " + ex.Message);
+                    Console.WriteLine();
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Enter a value greater than zero. Error: " + ex.Message);
+                    Console.WriteLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Something went wrong. Error: " + ex.Message);
+                    Console.WriteLine();
+                }
+            }
+            while (milesInt <= 0 || speedInt <= 0);
         }
     }
 }
